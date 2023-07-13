a3258025f95c6cb9f78cd934088cd78c01f8b69e
rehanvip <134266179+rehanvip@users.noreply.github.com>
2023-07-14 02:29:51 +0800

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -90,7 +90,7 @@ echo -e "$green      Install SSH / WS               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/bagusid93/ww/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 2
 clear
 wget https://raw.githubusercontent.com/rehanvip/xray-ws/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
